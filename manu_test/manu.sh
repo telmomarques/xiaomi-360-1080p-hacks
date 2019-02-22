14f853d6b2fa3a2c5c57833b7d28cb38e4442471
Telmo Marques <telmofmm@gmail.com>
2019-02-22 19:27:25 +0000

    Changed repo structure and added additional resources (images, firmware)

--- manu_test/manu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Xiaomi 360 Hacks enabled"
-rm -rf /tmp/factory_mode
-
-#Go only 1 level deep
-for f in /mnt/sdcard/x360h/*/*.sh ; do
-	sh $f
-done;
--- x360h/telnetd/telnetd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "Starting telnetd"
-/mnt/sdcard/x360h/busybox/busybox telnetd
